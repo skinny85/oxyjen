65dbb3674dbceb3bd0789aaa0f61778d24aac5c9
Adam Ruka <adamruka85@gmail.com>
2014-11-25 00:42:15 +0100

    A working prototype of setting a Java value from a JavaScript function.

--- src/main/scala/org/oxidize/Main.scala
+++ src/main/scala/org/oxidize/Main.scala
@@ -7,6 +7,11 @@ import javax.script.{ScriptContext, ScriptEngineManager, SimpleScriptContext}
 import org.apache.commons.io.FileUtils
 import org.slf4j.LoggerFactory
 
+@FunctionalInterface
+trait MyFunction[T, R] {
+  def apply(t: T): R
+}
+
 object Main {
   private val errLog = LoggerFactory.getLogger("org.oxidize.Main")
 
@@ -26,6 +31,11 @@ object Main {
     val template = args(0)
     val targetDir = if (args.length > 1) args(1) else "."
 
+    applyTemplate(template, targetDir)
+    0
+  }
+
+  def applyTemplate(template: String, targetDir: String) {
     val inString = FileUtils.readFileToString(new File(template))
     val regex = Pattern.compile("@\\{=([^{]+)\\}@")
     val matcher = regex.matcher(inString)
@@ -33,15 +43,25 @@ object Main {
     val nashorn = new ScriptEngineManager().getEngineByName("nashorn")
     val context = new SimpleScriptContext()
     context.setAttribute("groupId", "com.example.js", ScriptContext.ENGINE_SCOPE)
+    var customFileName: String = null
+    val setFileName = new MyFunction[String, String] {
+      def apply(str: String): String = {
+        customFileName = str
+        ""
+      }
+
+      override def toString: String = "SetFileName"
+    }
+
+    context.setAttribute("setFileName", setFileName, ScriptContext.ENGINE_SCOPE)
     while (matcher.find()) {
       val script = matcher.group(1)
       val result = nashorn.eval(script, context)
       matcher.appendReplacement(sb, String.valueOf(result))
     }
+    println("Custom file name is: '" + customFileName + "'")
     matcher.appendTail(sb)
     val outFile = new File(targetDir, template)
     FileUtils.writeStringToFile(outFile, sb.toString)
-
-    0
   }
 }
