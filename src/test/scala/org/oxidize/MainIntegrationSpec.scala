3979f0b4811ed2dfcb9fca4aae6f4003acce188a
Adam Ruka <adamruka85@gmail.com>
2014-11-30 19:03:44 +0100

    Extract a helper method in the Main integration test to reduce duplication.

--- src/test/scala/org/oxidize/MainIntegrationSpec.scala
+++ src/test/scala/org/oxidize/MainIntegrationSpec.scala
@@ -6,39 +6,57 @@ import org.apache.commons.io.FileUtils
 import org.oxidize.test.AbstractUnitSpec
 
 class MainIntegrationSpec extends AbstractUnitSpec {
-  "Main" should "generate a pom according to template" in {
+  "Main" should "generate a POM according to template" in {
     val testDir = "target/pom-test"
+    checkOxidizer(testDir = testDir,
+      template =
+        """@{ $oxidize.setFileName('pom.xml') }@
+          |<project>
+          |    <modelVersion>4.0.0</modelVersion>
+          |    <artifactId>artifactId</artifactId>
+          |    <groupId>
+          |        @{=
+          |            var prefix = 'pre.';
+          |            prefix + groupId + ".post"
+          |        }@
+          |    </groupId>
+          |    <version>1.0</version>
+          |</project>
+          |""".stripMargin,
+      outDir = testDir,
+      outFile = Some("pom.xml"),
+      expected =
+        """<project>
+          |    <modelVersion>4.0.0</modelVersion>
+          |    <artifactId>artifactId</artifactId>
+          |    <groupId>
+          |        pre.com.example.js.post
+          |    </groupId>
+          |    <version>1.0</version>
+          |</project>
+          |""".stripMargin,
+      "groupId=com.example.js")
+  }
+
+  private def checkOxidizer(testDir: String,
+                    template: String,
+                    outDir: String,
+                    outFile: Option[String],
+                    expected: String,
+                    args: String*) {
     ensureTestDirIsEmpty(testDir)
-    val template =
-      """|@{ $oxidize.setFileName('pom.xml') }@
-        |<project>
-        |    <modelVersion>4.0.0</modelVersion>
-        |    <artifactId>artifactId</artifactId>
-        |    <groupId>
-        |        @{=
-        |            var prefix = 'pre.';
-        |            prefix + groupId + ".post"
-        |        }@
-        |    </groupId>
-        |    <version>1.0</version>
-        |</project>
-        |""".stripMargin
-    val templateFilePath = testDir + "/pom.ejs.xml"
+
+    val templateFilePath = testDir + "/template.txt"
     FileUtils.writeStringToFile(new File(templateFilePath), template)
 
-    Main._main(Seq(templateFilePath, testDir, "groupId=com.example.js")) should be (0)
+    Main._main(Seq(templateFilePath, outDir) ++ Seq(args: _*)) should be(0)
 
-    val output = FileUtils.readFileToString(new File(testDir + "/pom.xml"))
-    output should be(
-      """|<project>
-        |    <modelVersion>4.0.0</modelVersion>
-        |    <artifactId>artifactId</artifactId>
-        |    <groupId>
-        |        pre.com.example.js.post
-        |    </groupId>
-        |    <version>1.0</version>
-        |</project>
-        |""".stripMargin)
+    val outFileName = outFile match {
+      case Some(o) => o
+      case None => "template.txt"
+    }
+    val output = FileUtils.readFileToString(new File(outDir + "/" + outFileName))
+    output should be(expected)
   }
 
   private def ensureTestDirIsEmpty(path: String): Unit = {
