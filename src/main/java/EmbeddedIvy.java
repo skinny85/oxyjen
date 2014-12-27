2a3dd05c6d5048d68d6962c09d02b2a00a9ea083
Adam Ruka <adamruka85@gmail.com>
2014-12-27 20:01:53 +0100

    Programatically using Ivy - using my own repo (requires a running Artifactory instance).

--- src/main/java/EmbeddedIvy.java
+++ src/main/java/EmbeddedIvy.java
@@ -1,13 +1,11 @@
-
 import org.apache.ivy.Ivy;
 import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
 import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
 import org.apache.ivy.core.module.id.ModuleRevisionId;
+import org.apache.ivy.core.report.ResolveReport;
 import org.apache.ivy.core.resolve.ResolveOptions;
 import org.apache.ivy.core.settings.IvySettings;
-import org.apache.ivy.plugins.resolver.URLResolver;
-import org.apache.ivy.core.report.ResolveReport;
-import org.apache.ivy.plugins.parser.xml.XmlModuleDescriptorWriter;
+import org.apache.ivy.plugins.resolver.IBiblioResolver;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,50 +15,53 @@ public class EmbeddedIvy {
     public static File resolveArtifact(String groupId, String artifactId, String version) throws IOException, ParseException {
         //creates clear ivy settings
         IvySettings ivySettings = new IvySettings();
+
         //url resolver for configuration of maven repo
-        URLResolver resolver = new URLResolver();
-        resolver.setM2compatible(true);
-        resolver.setName("central");
-        //you can specify the url resolution pattern strategy
+//        URLResolver resolver = new URLResolver();
+//        resolver.setM2compatible(true);
+//        resolver.setName("central");
+//        //you can specify the url resolution pattern strategy
 //        resolver.addArtifactPattern(
 //                "http://repo1.maven.org/maven2/[organisation]/[module]/[revision]/[artifact](-[revision]).[ext]");
+
+        IBiblioResolver resolver = new IBiblioResolver();
+        resolver.setM2compatible(true);
+        resolver.setName("oxyjen");
+        resolver.setRoot("http://localhost:8081/artifactory/oxyjen");
+
         //adding maven repo resolver
         ivySettings.addResolver(resolver);
         //set to the default resolver
         ivySettings.setDefaultResolver(resolver.getName());
-        //creates an Ivy instance with settings
-        Ivy ivy = Ivy.newInstance(ivySettings);
 
-        File ivyfile = File.createTempFile("ivy", ".xml");
-        ivyfile.deleteOnExit();
+        DefaultModuleDescriptor md = DefaultModuleDescriptor.newDefaultInstance(
+                ModuleRevisionId.newInstance(
+                        groupId, artifactId + "-caller", "working"));
 
-        String[] dep = new String[]{groupId, artifactId, version};
+        DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(
+                md,
+                ModuleRevisionId.newInstance(groupId, artifactId, version),
+                false, false, true);
+        dd.addDependencyConfiguration("default", "default");
 
-        DefaultModuleDescriptor md =
-                DefaultModuleDescriptor.newDefaultInstance(ModuleRevisionId.newInstance(dep[0],
-                        dep[1] + "-caller", "working"));
-
-        DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(md,
-                ModuleRevisionId.newInstance(dep[0], dep[1], dep[2]), false, false, true);
         md.addDependency(dd);
 
-        //creates an ivy configuration file
-        XmlModuleDescriptorWriter.write(md, ivyfile);
+        ResolveOptions resolveOptions = new ResolveOptions()
+                .setConfs(new String[]{"default"});
 
-        String[] confs = new String[]{"default"};
-        ResolveOptions resolveOptions = new ResolveOptions().setConfs(confs);
-
+        //creates an Ivy instance with settings
+        Ivy ivy = Ivy.newInstance(ivySettings);
         //init resolve report
-        ResolveReport report = ivy.resolve(ivyfile.toURL(), resolveOptions);
+        ResolveReport report = ivy.resolve(md, resolveOptions);
 
         //so you can get the jar library
-        File jarArtifactFile = report.getAllArtifactsReports()[0].getLocalFile();
 
-        return jarArtifactFile;
+        return report.getAllArtifactsReports()[0].getLocalFile();
     }
 
     public static void main(String[] args) throws IOException, ParseException {
         File jarArtifactFile = resolveArtifact("log4j", "log4j", "1.2.16");
+
         System.out.println("jarArtifactFile = " + jarArtifactFile.getAbsolutePath());
     }
 }
