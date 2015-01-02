750d4a79b147592a2e6e35f4d163fd23666643af
Adam Ruka <adamruka85@gmail.com>
2015-01-02 21:02:17 +0100

    Add an integration test verifying generating a correct Maven project.

--- src/test/scala/org/oxyjen/MainIntegrationSpec.scala
+++ src/test/scala/org/oxyjen/MainIntegrationSpec.scala
@@ -10,7 +10,7 @@ class MainIntegrationSpec extends AbstractUnitSpec {
 
   "Main" should "generate a POM according to template" in {
     val testDir = testsTmpDir + "pom-test"
-    callAndVerifyGeneration(testDir = testDir,
+    writeTemplateAndGenerate(testDir = testDir,
       template =
         """@{ $o2.setFileName('pom.xml') }@
           |<project>
@@ -25,7 +25,7 @@ class MainIntegrationSpec extends AbstractUnitSpec {
           |    <version>1.0</version>
           |</project>
           |""".stripMargin,
-      inFile = "template.txt",
+      templateFileName = "template.txt",
       mainFirstArg = None,
       outDir = testDir,
       outFile = Some("pom.xml"),
@@ -44,9 +44,9 @@ class MainIntegrationSpec extends AbstractUnitSpec {
 
   it should "create the output directory when it doesn't exist" in {
     val testDir = testsTmpDir + "new-dir-test"
-    callAndVerifyGeneration(testDir = testDir,
+    writeTemplateAndGenerate(testDir = testDir,
       template = "@{= a + b }@\n",
-      inFile = "template.txt",
+      templateFileName = "template.txt",
       mainFirstArg = None,
       outDir = testDir + "/a/b",
       outFile = None,
@@ -56,7 +56,7 @@ class MainIntegrationSpec extends AbstractUnitSpec {
 
   it should "allow setting target directory in the script" in {
     val testDir = testsTmpDir + "out-dir-test"
-    callAndVerifyGeneration(testDir = testDir,
+    writeTemplateAndGenerate(testDir = testDir,
       template =
         """abc reversed is '@{= 'abc'.split('').reverse().join('') }@'.
           |@{
@@ -64,7 +64,7 @@ class MainIntegrationSpec extends AbstractUnitSpec {
           |  $o2.setFileName(parts.join("/") + "/" + fileName);
           |}@
           |""".stripMargin,
-      inFile = "template.txt",
+      templateFileName = "template.txt",
       mainFirstArg = None,
       outDir = testDir,
       outFile = Some("a/b/c/output.txt"),
@@ -73,37 +73,122 @@ class MainIntegrationSpec extends AbstractUnitSpec {
   }
 
   it should "call all templates recursively when directory is given as argument" in {
-    val testDir = testsTmpDir + "recur-test"
+    val testDir = testsTmpDir + "recur-test/"
     ensureTestDirIsEmpty(testDir)
-    val template1 = testDir + "/" + "templ1.txt"
-    FileUtils.writeStringToFile(new File(template1), "@{= 1 + 2 }@")
-    val template2 = testDir + "/" + "templ2.txt"
-    FileUtils.writeStringToFile(new File(template2), "3 + 4")
-    val outDir = testDir + "/out"
+    FileUtils.writeStringToFile(new File(testDir + "templ1.txt"), "@{= 1 + 2 }@")
+    FileUtils.writeStringToFile(new File(testDir + "templ2.txt"), "3 + 4")
 
+    val outDir = testDir + "/out/"
     Main._main(Seq(testDir, outDir)) should be (0)
-    FileUtils.readFileToString(new File(outDir + "/templ1.txt")) should be ("3\n")
-    FileUtils.readFileToString(new File(outDir + "/templ2.txt")) should be ("3 + 4\n")
+
+    fileContentsShouldBe(outDir + "templ1.txt", "3\n")
+    fileContentsShouldBe(outDir + "templ2.txt", "3 + 4\n")
   }
 
   it should "preserve the original location of the file" in {
     val testDir = testsTmpDir + "flatten-test"
-    callAndVerifyGeneration(testDir = testDir,
+    writeTemplateAndGenerate(testDir = testDir,
       template =
         """
           |abc
           |
           |""".stripMargin,
-      inFile = "META-INF/manifest.mf",
+      templateFileName = "META-INF/manifest.mf",
       mainFirstArg = Some(testDir),
       outDir = testDir + "/out",
       outFile = Some("META-INF/manifest.mf"),
       expected = "abc\n")
   }
 
-  private def callAndVerifyGeneration(testDir: String,
+  it should "generate correct Maven project" in {
+    val testDir = testsTmpDir + "maven-test"
+    ensureTestDirIsEmpty(testDir)
+    val inDir = testDir + "/in/"
+    val outDir = testDir + "/out/"
+
+    val pomFormat =
+      """<project>
+        |    <modelVersion>4.0.0</modelVersion>
+        |    <groupId>%s</groupId>
+        |    <artifactId>%s</artifactId>
+        |    <version>1.0</version>
+        |    <packaging>jar</packaging>
+        |
+        |    <dependencies>
+        |      <dependency>
+        |        <groupId>junit</groupId>
+        |        <artifactId>junit</artifactId>
+        |        <version>4.11</version>
+        |      </dependency>
+        |    </dependencies>
+        |</project>
+        |""".stripMargin
+    FileUtils.writeStringToFile(
+      new File(inDir + "pom.xml"),
+      String.format(pomFormat, "@{= groupId }@", "@{= projectId }@"))
+
+    val manifest =
+      """Manifest-Version: 1.0
+        |Created-By: 1.3.1 (Apple Computer, Inc.)
+        |""".stripMargin
+    FileUtils.writeStringToFile(
+      new File(inDir + "META-INF/MANIFEST.MF"),
+      manifest)
+
+    val classFormat =
+      """%spackage %s;
+        |
+        |public class ExampleClass {
+        |}
+        |""".stripMargin
+    FileUtils.writeStringToFile(
+      new File(inDir + "src/main/java/ExampleClass.java"),
+      String.format(classFormat,
+        """@{
+          |        var pathParts = groupId.split('.');
+          |        $o2.setFileName(pathParts.join('/') + '/ExampleClass.java');
+          |}@
+          |""".stripMargin,
+        "@{= groupId }@"))
+
+
+    val testFormat =
+      """%spackage %s;
+        |
+        |import org.junit.Test;
+        |import org.junit.Assert;
+        |
+        |public class ExampleTest {
+        |    @Test
+        |    public void example_test() {
+        |        Assert.assertEquals(3, 1 + 2);
+        |    }
+        |}
+        |""".stripMargin
+    FileUtils.writeStringToFile(
+      new File(inDir + "src/test/java/ExampleTest.java"),
+      String.format(testFormat,
+        """@{
+          |        var pathParts = groupId.split('.');
+          |        $o2.setFileName(pathParts.join('/') + '/ExampleTest.java');
+          |}@
+          |""".stripMargin,
+        "@{= groupId }@"))
+
+    Main._main(Seq(inDir, outDir, "groupId=org.example", "projectId=example")) should be (0)
+
+    fileContentsShouldBe(outDir + "META-INF/MANIFEST.MF", manifest)
+    fileContentsShouldBe(outDir + "pom.xml",
+      String.format(pomFormat, "org.example", "example"))
+    fileContentsShouldBe(outDir + "src/main/java/org/example/ExampleClass.java",
+      String.format(classFormat, "", "org.example"))
+    fileContentsShouldBe(outDir + "src/test/java/org/example/ExampleTest.java",
+      String.format(testFormat, "", "org.example"))
+  }
+
+  private def writeTemplateAndGenerate(testDir: String,
                                       template: String,
-                                      inFile: String,
+                                      templateFileName: String,
                                       mainFirstArg: Option[String],
                                       outDir: String,
                                       outFile: Option[String],
@@ -111,14 +196,12 @@ class MainIntegrationSpec extends AbstractUnitSpec {
                                       args: String*) {
     ensureTestDirIsEmpty(testDir)
 
-    val templateFilePath = testDir + "/" + inFile
+    val templateFilePath = testDir + "/" + templateFileName
     FileUtils.writeStringToFile(new File(templateFilePath), template)
 
     Main._main(Seq(mainFirstArg.getOrElse(templateFilePath), outDir) ++ Seq(args: _*)) should be(0)
 
-    val output = FileUtils.readFileToString(new File(outDir + "/" +
-      outFile.getOrElse(inFile)))
-    output should be (expected)
+    fileContentsShouldBe(outDir + "/" + outFile.getOrElse(templateFileName), expected)
   }
 
   private def ensureTestDirIsEmpty(path: String): Unit = {
@@ -127,5 +210,9 @@ class MainIntegrationSpec extends AbstractUnitSpec {
       FileUtils.deleteDirectory(testDir)
     }
     testDir.mkdirs()
+  }
+
+  def fileContentsShouldBe(filePath: String, expected: String) {
+    FileUtils.readFileToString(new File(filePath)) should be(expected)
   }
 }
