62fb5f5f123c4003945a104e356dbb5a6447ac92
Adam Ruka <adamruka85@gmail.com>
2015-01-02 21:28:40 +0100

    Remove unnecessary test Ivy program.

--- src/main/scala/org/oxyjen/ivy/IvyTestMain.scala
+++
@@ -1,8 +0,0 @@
-package org.oxyjen.ivy
-
-object IvyTestMain {
-  def main(args: Array[String]): Unit = {
-    println("File from Ivy: " + IvyResolver.resolve("org.apache", "log4j2", "2.1"))
-    println("File from Ivy: " + IvyResolver.resolve("log4j", "log4j", "1.2.16"))
-  }
-}
