2a323364c3f6dc31c3541acf68b3fb27d59460da
Adam Ruka <adamruka85@gmail.com>
2015-01-15 23:29:49 +0100

    Added a stub login action using a redirect with Flash message.

--- oxyjen-www/app/controllers/OzoneApplication.scala
+++ oxyjen-www/app/controllers/OzoneApplication.scala
@@ -3,7 +3,11 @@ package controllers
 import play.api.mvc._
 
 object OzoneApplication extends Controller {
-  def index = Action {
+  def index = Action { implicit request =>
     Ok(views.html.ozone.index())
+  }
+
+  def login = Action { implicit request =>
+    Redirect(routes.OzoneApplication.index()).flashing(("message", "You have been logged in"))
   }
 }
