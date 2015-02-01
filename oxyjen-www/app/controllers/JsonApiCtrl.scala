6a2fdc388429a96d7effda48348584d040a3b670
Adam Ruka <adamruka85@gmail.com>
2015-02-01 15:02:16 +0100

    Add a Register JSON API endpoint.

--- oxyjen-www/app/controllers/JsonApiCtrl.scala
+++ oxyjen-www/app/controllers/JsonApiCtrl.scala
@@ -1,9 +1,8 @@
 package controllers
 
-import play.api.libs.json._
 import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import models._
 
@@ -12,5 +11,30 @@ object JsonApiCtrl extends Controller {
   def testJson = Action { implicit  request =>
     val json = Json.obj("key" -> "val")
     Ok(json)
+  }
+
+  case class RegisterJson(orgId: String, password: String)
+
+  implicit val registerJsonReads: Reads[RegisterJson] = (
+    (JsPath \ "orgId").read[String] and
+    (JsPath \ "password").read[String]
+  )(RegisterJson.apply _)
+
+  def register = Action(BodyParsers.parse.json) { implicit request =>
+    val registerJsonBind = request.body.validate[RegisterJson]
+    registerJsonBind.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "ERROR", "message" -> JsError.toFlatJson(errors)))
+      },
+      registerJson => {
+        OrganizationRepository.create(registerJson.orgId, registerJson.password) match {
+          case InvalidOrgArguments(violations) =>
+            UnprocessableEntity(Json.obj("status" -> "ERROR", "message" -> "Invalid arguments",
+              "violations" -> violations.map(_.message)))
+          case SuccessfulOrgCreation(id) =>
+            Created(Json.obj("status" -> "OK", "message" -> "Organization created"))
+        }
+      }
+    )
   }
 }
