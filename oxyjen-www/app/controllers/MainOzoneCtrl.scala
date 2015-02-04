a34be689affc59a7009a8b83a5b005b1c4a8a57f
Adam Ruka <adamruka85@gmail.com>
2015-02-04 15:15:32 +0100

    Add a separate Sign In page.

--- oxyjen-www/app/controllers/MainOzoneCtrl.scala
+++ oxyjen-www/app/controllers/MainOzoneCtrl.scala
@@ -49,4 +49,27 @@ object MainOzoneCtrl extends Controller {
   def logout = Action { implicit request =>
     CtrlSecurityUtil.logout(routes.MainOzoneCtrl.index())
   }
+
+  def loginPage(orgId: String = "") = Action { implicit request =>
+    Ok(views.html.ozone.login(orgId))
+  }
+
+  def loginPagePost = Action { implicit request =>
+    val boundLoginForm = loginForm.bindFromRequest()
+    boundLoginForm.fold(
+      loginFormWithErrors => {
+        BadRequest(views.html.ozone.login(loginFormWithErrors("orgId").value.getOrElse("")))
+      },
+      loginFormViewModel => {
+        OzoneSecurity.login(loginFormViewModel.orgId, loginFormViewModel.password) match {
+          case Some(tksid) =>
+            Redirect(routes.MainOzoneCtrl.index())
+              .withSession("tksid" -> tksid)
+          case None =>
+            Redirect(routes.MainOzoneCtrl.loginPage(loginFormViewModel.orgId))
+              .flashing(("error", "Incorrect Organization ID and/or password"))
+        }
+      }
+    )
+  }
 }
