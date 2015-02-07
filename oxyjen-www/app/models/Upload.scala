a38fa5723801cc228e26ced5da7f003f4d45ffa6
Adam Ruka <adamruka85@gmail.com>
2015-02-07 17:32:39 +0100

    Use Scala XML literals instead of temporary files for uploading POM.

--- oxyjen-www/app/models/Upload.scala
+++ oxyjen-www/app/models/Upload.scala
@@ -74,26 +74,24 @@ object Upload {
         Logger.info("Upload response:\n" + resp.json.toString())
     }
 
-    val pomFile = File.createTempFile("pom-", ".xml")
-    FileUtils.writeStringToFile(pomFile,
-      """<?xml version="1.0" encoding="UTF-8"?>
-        <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <modelVersion>4.0.0</modelVersion>
+    val pomXml =
+<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+         xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <modelVersion>4.0.0</modelVersion>
 
-          <groupId>""" + org.orgId + """</groupId>
-          <artifactId>""" + name + """</artifactId>
-          <version>""" + version + """</version>
-          <packaging>zip</packaging>
-          <description>Artifactory auto generated POM</description>
-        </project>
-        """)
+    <groupId>{org.orgId}</groupId>
+    <artifactId>{name}</artifactId>
+    <version>{version}</version>
+    <packaging>zip</packaging>
+    <description>Artifactory auto generated POM</description>
+</project>
 
     val holder2 = WS.
       url(s"http://localhost:8081/artifactory/oxyjen/${org.orgId}/$name/$version/$name-$version.pom").
       withAuth("admin", "password", WSAuthScheme.BASIC)
 
-    val result2 = holder2.put(pomFile)
+    val result2 = holder2.put(pomXml)
     result2.onComplete {
       case Failure(e) =>
         Logger.error("Error uploading file", e)
