8f9cc2cf9f83f36c53df62ce8dda3c7ba7486f24
Adam Ruka <adamruka85@gmail.com>
2015-03-09 00:26:22 +0100

    Removed duplication from OZone REST client.

--- oxyjen-cli/src/main/scala/org/oxyjen/ozone/rest/OZoneRestClient.scala
+++ oxyjen-cli/src/main/scala/org/oxyjen/ozone/rest/OZoneRestClient.scala
@@ -14,14 +14,12 @@ import scala.concurrent.duration.Duration
 object OZoneRestClient {
   def register(orgId: String, password: String): Either[Throwable, RegisterResponseJson] = {
     val req = requestFor("register").POST.setBody(requestOrgJson(orgId, password))
-    val future = Http(req > RegisterDispatchHandler).either
-    Await.result(future, Duration.Inf)
+    performRequest(req, RegisterDispatchHandler)
   }
 
   def login(orgId: String, password: String): Either[Throwable, LoginResponseJson] = {
     val req = requestFor("login").POST.setBody(requestOrgJson(orgId, password))
-    val future = Http(req > LoginDispatchHandler).either
-    Await.result(future, Duration.Inf)
+    performRequest(req, LoginDispatchHandler)
   }
 
   def upload(token: String, name: String, version: String, file: File): Either[Throwable, UploadResponseJson] = {
@@ -31,14 +29,12 @@ object OZoneRestClient {
         "name" -> Seq(name),
         "version" -> Seq(version)))
       .<<<(file)
-    val future = Http(req > UploadDispatchHandler).either
-    Await.result(future, Duration.Inf)
+    performRequest(req, UploadDispatchHandler)
   }
 
   def search(term: String): Either[Throwable, SearchResponseJson] = {
     val req = requestFor("search").setBody(s"""{"term": "$term"}""")
-    val future = Http(req > SearchDispatchHandler).either
-    Await.result(future, Duration.Inf)
+    performRequest(req, SearchDispatchHandler)
   }
 
   private def requestFor(function: String): Req = {
@@ -49,87 +45,71 @@ object OZoneRestClient {
   private def requestOrgJson(orgId: String, password: String): String = {
     s"""{"orgId": "$orgId", "password": "$password"}"""
   }
+
+  private def performRequest[R](req: Req, handler: Response => R): Either[Throwable, R] = {
+    Await.result(Http(req > handler).either, Duration.Inf)
+  }
 }
 
-object RegisterDispatchHandler extends (Response => RegisterResponseJson) {
-  override def apply(resp: Response): RegisterResponseJson = {
-    val stringResponse = resp.getResponseBody
-    val json = parse(stringResponse)
-    resp.getStatusCode match {
-      case 400 =>
-        json.extract[ClientErrorJson]
-      case 422 =>
-        json.extract[InvalidArgumentsJson]
-      case 201 =>
-        json.extract[OrgCreatedJson]
-    }
+abstract class AbstractJsonDispatchHandler[R] extends (Response => R) {
+  final def apply(resp: Response): R = {
+    val stringResponseBody = resp.getResponseBody
+    implicit val json = parse(stringResponseBody)
+    handle(resp.getStatusCode)
   }
 
-  private implicit val formats = DefaultFormats
-}
-
-object LoginDispatchHandler extends (Response => LoginResponseJson) {
-  override def apply(resp: Response): LoginResponseJson = {
-    val stringResponse = resp.getResponseBody
-    val json = parse(stringResponse)
-    resp.getStatusCode match {
-      case 400 =>
-        json.extract[ClientErrorJson]
-      case 401 =>
-        json.extract[InvalidCredentialsJson]
-      case 200 =>
-        json.extract[LoginSuccessfulJson]
-    }
+  protected final def extractJson[T](implicit json: JValue, manifest: Manifest[T]): T = {
+    json.extract[T]
   }
 
-  private implicit val formats = DefaultFormats
+  protected def handle(statusCode: Int)(implicit json: JValue): R
+
+  protected implicit val formats = DefaultFormats
 }
 
-object UploadDispatchHandler extends (Response => UploadResponseJson) {
-  override def apply(resp: Response): UploadResponseJson = {
-    val stringResponse = resp.getResponseBody
-    val json = parse(stringResponse)
-    resp.getStatusCode match {
-      case 400 =>
-        json.extract[ClientErrorJson]
-      case 500 =>
-        json.extract[ServerErrorJson]
-      case 422 =>
-        json.extract[InvalidArgumentsJson]
-      case 401 =>
-        json.extract[UnauthorizedJson]
-      case 200 =>
-        json.extract[FileUploadedJson]
-    }
+object RegisterDispatchHandler extends AbstractJsonDispatchHandler[RegisterResponseJson] {
+  protected def handle(statusCode: Int)(implicit json: JValue): RegisterResponseJson = statusCode match {
+    case 400 => extractJson[ClientErrorJson]
+    case 422 => extractJson[InvalidArgumentsJson]
+    case 201 => extractJson[OrgCreatedJson]
   }
-
-  private implicit val formats = DefaultFormats
 }
 
-object SearchDispatchHandler extends (Response => SearchResponseJson) {
-  override def apply(resp: Response): SearchResponseJson = {
-    val stringResponse = resp.getResponseBody
-    val json = parse(stringResponse)
-    resp.getStatusCode match {
-      case 400 =>
-        json.extract[ClientErrorJson]
-      case 500 =>
-        json.extract[ServerErrorJson]
-      case 200 =>
-        json.extract[SearchResultsJson]
-    }
+object LoginDispatchHandler extends AbstractJsonDispatchHandler[LoginResponseJson] {
+  protected def handle(statusCode: Int)(implicit json: JValue): LoginResponseJson = statusCode match {
+    case 400 => extractJson[ClientErrorJson]
+    case 401 => extractJson[InvalidCredentialsJson]
+    case 200 => extractJson[LoginSuccessfulJson]
   }
-
-  private implicit val formats = DefaultFormats
 }
 
-sealed trait RegisterResponseJson
+object UploadDispatchHandler extends AbstractJsonDispatchHandler[UploadResponseJson] {
+  protected def handle(statusCode: Int)(implicit json: JValue): UploadResponseJson = statusCode match {
+    case 400 => extractJson[ClientErrorJson]
+    case 500 => extractJson[ServerErrorJson]
+    case 422 => extractJson[InvalidArgumentsJson]
+    case 401 => extractJson[UnauthorizedJson]
+    case 200 => extractJson[FileUploadedJson]
+  }
+}
 
-sealed trait LoginResponseJson
+object SearchDispatchHandler extends AbstractJsonDispatchHandler[SearchResponseJson] {
+  protected def handle(statusCode: Int)(implicit json: JValue): SearchResponseJson = statusCode match {
+    case 400 => extractJson[ClientErrorJson]
+    case 500 => extractJson[ServerErrorJson]
+    case 200 => extractJson[SearchResultsJson]
+  }
+}
 
-sealed trait UploadResponseJson
+sealed trait OZoneResponseJson
 
-sealed trait SearchResponseJson
+sealed trait RegisterResponseJson extends OZoneResponseJson
+
+sealed trait LoginResponseJson extends OZoneResponseJson
+
+sealed trait UploadResponseJson extends OZoneResponseJson
+
+sealed trait SearchResponseJson extends OZoneResponseJson
 
 final case class ClientErrorJson(status: String, message: String)
   extends RegisterResponseJson
