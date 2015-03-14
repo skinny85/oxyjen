bc20c76162a00e93685e956aca65dda429598886
Adam Ruka <adamruka85@gmail.com>
2015-03-15 00:14:59 +0100

    Remove duplication in JsonApiCtrl.

--- oxyjen-www/app/controllers/JsonApiCtrl.scala
+++ oxyjen-www/app/controllers/JsonApiCtrl.scala
@@ -2,8 +2,9 @@ package controllers
 
 import models._
 import models.util.Futures
+import play.api.data.validation.ValidationError
+import play.api.libs.json.Json.JsValueWrapper
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
 import play.api.mvc._
 
 import scala.concurrent.Future
@@ -11,28 +12,18 @@ import scala.util.Either.RightProjection
 import scala.util.{Failure, Success}
 
 object JsonApiCtrl extends Controller {
-
-  case class OrganizationJson(orgId: String, password: String)
-
-  implicit val organizationJsonReads: Reads[OrganizationJson] = (
-      (JsPath \ "orgId").read[String] and
-      (JsPath \ "password").read[String]
-    )(OrganizationJson.apply _)
+  private case class OrganizationJson(orgId: String, password: String)
 
   def register = Action(BodyParsers.parse.json) { implicit request =>
     val organizationJsonBind = request.body.validate[OrganizationJson]
     organizationJsonBind.fold(
-      errors => {
-        BadRequest(Json.obj("status" -> "ERROR", "message" -> JsError.toFlatJson(errors)))
-      },
+      errors => incorrectJsonFormat(errors),
       organizationJson => {
         OrganizationRepository.create(organizationJson.orgId, organizationJson.password) match {
           case Left(violations) =>
-            UnprocessableEntity(Json.obj("status" -> "ERROR", "message" -> "Invalid arguments",
-              "violations" -> violations.map(_.message)))
+            constraintViolation(violations)
           case Right(tksid) =>
-            Created(Json.obj("status" -> "OK", "message" -> "Organization created",
-              "tksid" -> tksid))
+            Created(authenticatedJson(tksid, "Organization created"))
         }
       }
     )
@@ -41,18 +32,20 @@ object JsonApiCtrl extends Controller {
   def login = Action(BodyParsers.parse.json) { implicit request =>
     val organizationJsonBind = request.body.validate[OrganizationJson]
     organizationJsonBind.fold(
-      errors => {
-        BadRequest(Json.obj("status" -> "ERROR", "message" -> JsError.toFlatJson(errors)))
-      },
+      errors => incorrectJsonFormat(errors),
       organizationJson => {
         OzoneSecurity.login(organizationJson.orgId, organizationJson.password) match {
           case None =>
-            Unauthorized(Json.obj("status" -> "ERROR", "message" -> "Invalid credentials supplied"))
+            unauthorized("Invalid credentials supplied")
           case Some(tksid) =>
-            Ok(Json.obj("status" -> "OK", "message" -> "Authentication successful", "tksid" -> tksid))
+            Ok(authenticatedJson(tksid, "Authentication successful"))
         }
       }
     )
+  }
+
+  private def authenticatedJson(tksid: String, msg: String): JsObject = {
+    Json.obj("status" -> "OK", "message" -> msg, "tksid" -> tksid)
   }
 
   def upload = Action.async(parse.temporaryFile) { implicit request =>
@@ -62,21 +55,18 @@ object JsonApiCtrl extends Controller {
       version <- requiredRequestParam("version")
     } yield (tksid, name, version)) match {
       case Left(msg) =>
-        Future.successful(BadRequest(Json.obj("status" -> "ERROR", "message" -> msg)))
+        Future.successful(badRequest(msg))
       case Right((tksid, name, version)) =>
         OzoneSecurity.verifyToken(tksid) match {
           case None =>
-            Future.successful(Unauthorized(Json.obj("status" -> "ERROR", "message" -> "Invalid token")))
+            Future.successful(unauthorized("Invalid token"))
           case Some(org) =>
             Futures.mapTry(Upload.upload(org, name, version, request.body.file)) {
               case Failure(e) =>
-                InternalServerError(Json.obj("status" -> "ERROR", "message" -> e.getMessage))
+                unknownError(e)
               case Success(either) => either match {
                 case Left(violations) =>
-                  UnprocessableEntity(Json.obj(
-                    "status" -> "ERROR",
-                    "message" -> "Invalid arguments",
-                    "violations" -> violations.map(_.message)))
+                  constraintViolation(violations)
                 case Right(_) =>
                   Ok(Json.obj("status" -> "OK", "message" -> "Archive uploaded"))
               }
@@ -85,14 +75,19 @@ object JsonApiCtrl extends Controller {
     }
   }
 
+  private def requiredRequestParam[A](param: String)(implicit req: Request[A]): RightProjection[String, String] = {
+    req.getQueryString(param).toRight(s"Missing required parameter '$param'").right
+  }
+
+  private case class SearchQueryJson(term: String)
+
   def search = Action.async(BodyParsers.parse.json) { implicit request =>
     request.body.validate[SearchQueryJson].fold(
-      errors => Future.successful(
-        BadRequest(Json.obj("status" -> "ERROR", "message" -> JsError.toFlatJson(errors)))),
+      errors => Future.successful(incorrectJsonFormat(errors)),
       searchQueryJson => {
         Futures.mapTry(Artifacts.matching(searchQueryJson.term)) {
           case Failure(e) =>
-            InternalServerError(Json.obj("status" -> "ERROR", "message" -> e.getMessage))
+            unknownError(e)
           case Success(resultsMap) =>
             Ok(Json.obj(
               "status" -> "OK",
@@ -107,12 +102,28 @@ object JsonApiCtrl extends Controller {
     )
   }
 
-  private def requiredRequestParam[A](param: String)(implicit req: Request[A]): RightProjection[String, String] = {
-    req.getQueryString(param).toRight(s"Missing required parameter '$param'").right
+  private def incorrectJsonFormat(errors: Seq[(JsPath, Seq[ValidationError])]) = {
+    badRequest(JsError.toFlatJson(errors))
   }
 
-  private case class SearchQueryJson(term: String)
+  private def badRequest(msg: JsValueWrapper) = {
+    BadRequest(Json.obj("status" -> "ERROR", "message" -> msg))
+  }
 
+  private def constraintViolation(violations: ConstraintViolations) = {
+    UnprocessableEntity(Json.obj("status" -> "ERROR", "message" -> "Invalid arguments",
+      "violations" -> violations.map(_.message)))
+  }
+
+  private def unauthorized(msg: String) = {
+    Unauthorized(Json.obj("status" -> "ERROR", "message" -> msg))
+  }
+
+  private def unknownError(e: Throwable) = {
+    InternalServerError(Json.obj("status" -> "ERROR", "message" -> e.getMessage))
+  }
+
+  private implicit val organizationJsonReads = Json.reads[OrganizationJson]
   private implicit val searchQueryJsonReads = Json.reads[SearchQueryJson]
   private implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
 }
